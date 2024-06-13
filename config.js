63bd8e676d4a71faebd163f4f94713d6b96cf369
JasonChiang7kk <602092563@qq.com>
2024-06-13 16:24:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -29,11 +29,11 @@ var config = {
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        play: "俾D音乐",
+        bannar_coming: "加 D 野",
+        balloons_flying: "好似仲差D野",
+        cake_fadein: "嚟个蛋糕？",
+        light_candle: "点埋蜡烛？",
         wish_message: "生日快乐",
         story: "A MESSAGE FOR YOU",
     }
